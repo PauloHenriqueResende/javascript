a6bd340f970bb38240cf0bed74ef31ce1fff8043
Paulo Henrique Resende Rabelo <130231762+PauloHenriqueResende@users.noreply.github.com>
2023-05-23 20:35:22 -0300

    novos ex

---
+++ exercicios/ex007/ex007.js
@@ -0,0 +1,6 @@
+var altitude = 500
+if ( altitude <=20)
+{ console.log(' Você esta na troposfera')}
+ else   if ( altitude <=50 ) {console.log(' Você esta na Estratosfera')}  
+ else   if ( altitude >=80) { console.log('Você esta na Mesosfera')}  
+ else if  (altitude <=450) {console.log('Você esta na Termosfera')}
